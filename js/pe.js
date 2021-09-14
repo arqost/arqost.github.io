2d0ad9fca8d05bcb3a43a62226281927bd2872ab
Arq <angrod@o2.pl>
2021-09-14 15:38:01 +0200

    Update pe.js

--- js/pe.js
+++ js/pe.js
@@ -1,47 +1,48 @@
 "use strict";
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
+window.addEventListener('load', function () {
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
     }
-    return null;
-}
 
-function eraseCookie(name) {   
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
+    function eraseCookie(name) {   
+        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
 
-function isCookieEneble(name) {
-  var retVal = false;
-    // Quick test if browser has cookieEnabled host property
-  //if (navigator.cookieEnabled) { retVal = true };
-  // Create cookie
-  // document.cookie = "cookietest=1";
-  if ( retVal == false ) {
-      setCookie(name,1,1);
-      var retVal = document.cookie.indexOf(name) != -1;
-      // Delete cookie
-      // document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
-      eraseCookie(name);
-  }
-  console.log(retVal);
-  return retVal;
+    function isCookieEneble(name) {
+      var retVal = false;
+        // Quick test if browser has cookieEnabled host property
+      //if (navigator.cookieEnabled) { retVal = true };
+      // Create cookie
+      // document.cookie = "cookietest=1";
+      if ( retVal == false ) {
+          setCookie(name,1,1);
+          var retVal = document.cookie.indexOf(name) != -1;
+          // Delete cookie
+          // document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+          eraseCookie(name);
+      }
+      console.log(retVal);
+      return retVal;
+    }
 }
-
 
 function createPointSqare(x,y) {
     // <svg width="400" height="110">
